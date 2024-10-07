a16aa4e145f56dbffa5978c21cdf336c20aac2a3
ulayaw-rex <jhonrextabanas27@gmail.com>
2024-10-07 18:29:38 +0800

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h3 class="text-red-700 font-bold ">Hello, Rex</h3>
+
+                    <p class="bg-blue-400 p-8 rounded-lg text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo veritatis suscipit distinctio vitae rem tempore rerum vel qui accusantium doloribus!</p>
                 </div>
             </div>
+
         </div>
     </div>
 </x-app-layout>
